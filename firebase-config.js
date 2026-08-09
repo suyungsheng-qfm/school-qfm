72ff8c79b1e1eff1750f61613c13bf1855468de9
su-yung-sheng <suyungsheng@mail.qfm.kh.edu.tw>
2026-08-09 16:52:33 +0800

    設定 school-qfm Firebase 專案

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
-// 請將此檔案中的值替換為 Firebase 專案設定（Firebase Console > 專案設定）。
+// Firebase Console > 專案設定 > 一般 > 您的應用程式。
 // 若要公開網站，務必同時部署 firestore.rules 內的存取規則。
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBvrZlmh2KG9k6-PtdyVRwOLU9HXCU9gj0",
+  authDomain: "school-qfm.firebaseapp.com",
+  projectId: "school-qfm",
+  storageBucket: "school-qfm.firebasestorage.app",
+  messagingSenderId: "778929762533",
+  appId: "1:778929762533:web:9dc508ea85106d780a240c",
+  measurementId: "G-81SNBJM6J3"
 };
